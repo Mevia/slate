76b3e950381955d8413ab5d10bac3c8e0c54c759
Jesper Persson <jespert.persson@gmail.com>
2015-11-04 14:58:53 +0100

    Add raketask for precompile

--- Rakefile
+++ Rakefile
@@ -4,3 +4,9 @@ require 'rake/clean'
 CLOBBER.include('build')
 
 task :default => [:build]
+
+namespace :assets do
+  task :precompile do
+    sh 'middleman build'
+  end
+end
--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+require 'rack'
+require 'rack/contrib/try_static'
+
 # Markdown
 set :markdown_engine, :redcarpet
 set :markdown,
@@ -37,3 +40,9 @@ configure :build do
   # activate :asset_hash
   # activate :gzip
 end
+
+# Serve files from the build directory
+# use Rack::TryStatic,
+#   root: 'build',
+#   urls: %w[/],
+#   try: ['.html', 'index.html', '/index.html']
